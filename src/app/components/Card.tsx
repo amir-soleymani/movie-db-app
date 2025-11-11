ddcb57ea9ee0f37b1de89c6d307b3b913f275202
Amir Soleymani <amirsoleymanifarisar@gmail.com>
2025-11-11 19:34:00 +0100

    feat: Implement responsive grid and Movie Card component with Image optimization

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface CarProps {
+  result: any; //improve later
+}
+
+export default function Card({ result }: CardProps) {
+  // Base URL for TMDB images
+  const imageUrl = "https://image.tmdb.org/t/p/original";
+
+  return (
+    <Link href={`/movie/${result.id}`}>
+      <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border-slate-400 sm:m-2 transition-shadow duration-200">
+        {/* 1. Movie Poster Image */}
+        <Image
+          src={`${imageUrl}${result.backdrop_path || result.poster_path}`}
+          width={500}
+          height={300}
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+          }}
+          alt="movie poster"
+          className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-300"
+        />
+
+        {/* 2. Text Content */}
+        <div className="p-2">
+          <h2 className="truncate text-lg font-bold">
+            {result.title || result.name}
+          </h2>
+          <p className="flex items-center">
+            {result.release_date || result.first_air_date}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 // src/app/page.tsx
-
+import Card from "./components/Card";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export default async function Home({
@@ -33,13 +33,13 @@ export default async function Home({
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">{title}</h1>
-      {/* Ensure results is an array before mapping */}
-      {Array.isArray(results) &&
-        results.map((result: any) => (
-          <div key={result.id} className="border-b py-2">
-            <p className="text-lg">{result.title || result.name}</p>
-          </div>
-        ))}
+      <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
+        {/* Ensuring results is an array before mapping */}
+        {Array.isArray(results) &&
+          results.map((result: any) => (
+            <Card key={result.id} result={result} />
+          ))}
+      </div>
     </div>
   );
 }
